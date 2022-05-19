ab319cc3f95c242b5261f01c4a9516efb25b02fd
Slava Markeyev <slava@nextdoor.com>
2022-05-19 11:54:28 -0700

    allow creating a logger per connection

--- rediscluster/cluster.go
+++ rediscluster/cluster.go
@@ -187,8 +187,8 @@ func NewCluster(ctx context.Context, initAddrs []string, opts Opts) (*Cluster, e
 	}
 	cluster.ctx, cluster.cancel = context.WithCancel(ctx)
 
-	if cluster.opts.HostOpts.Logger == nil {
-		cluster.opts.HostOpts.Logger = defaultConnLogger{cluster}
+	if cluster.opts.HostOpts.LoggerFactory == nil {
+		cluster.opts.HostOpts.LoggerFactory = &DefaultLoggerFactory{cluster}
 	}
 	if cluster.opts.Logger == nil {
 		cluster.opts.Logger = DefaultLogger{}
